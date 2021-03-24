a18b8975e82b5833261f2115aec02b7ca6f35a1e
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-24 15:56:34 +0900

    map設定完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
+import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incomplateTodos, setIncomplateTodos] = useState(["もももも", "2"]);
+  const [complateTodos, setComplateTodo] = useState(["33", "hanamichi"]);
   return (
     <>
-      <div>
+      <div className="input_eria">
         <input placeholder="Todoを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplate_aria">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
-            <li>文字が入ります</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div>
-            <li>文字が入ります</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incomplateTodos.map((todo) => {
+            return (
+              <div key={todo} className="list_row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complate_aria">
+        <p className="title">完了のTODO</p>
         <ul>
-          <div>
-            <li>文字が入ります</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div>
-            <li>文字が入ります</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {complateTodos.map((todo) => {
+            return (
+              <div key={todo} className="list_row">
+                <li>{todo}</li>
+                <button>戻る</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
