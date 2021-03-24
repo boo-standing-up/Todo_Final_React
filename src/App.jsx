2622a59d2284d24a6a601a5b093f1ee37574c755
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-24 14:41:26 +0900

    完了UP

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,36 @@ export const App = () => {
         <input placeholder="Todoを入力" />
         <button>追加</button>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>文字が入ります</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>文字が入ります</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>文字が入ります</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>文字が入ります</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
