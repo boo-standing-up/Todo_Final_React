304cb7670450e8cd0bb3d825ca1010d3e0e7a731
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-24 14:31:09 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder="Todoを入力" />
+        <button>追加</button>
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  );
+};
